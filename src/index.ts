ed39eeefe0dcf070fea8122adf5530162aa5c4c6
datlai3042 <datlai304@gmail.com>
2025-06-06 10:00:30 +0700

    Config Project

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://core-stream-call-video.vercel.app'], // Cho phép truy cập từ origin này
+    origin: process.env.MODE === 'DEV' ? ['http://localhost:3000', 'http://localhost:5173',] : process.env.CLIENT_URL, // Cho phép truy cập từ origin này
 
     methods: ['GET', 'POST'], // Chỉ cho phép các phương thức GET và POST
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'], // Chỉ
@@ -45,7 +45,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://core-stream-call-video.vercel.app'], // Cho phép truy cập từ origin này
+    origin: ['http://localhost:3000', 'http://localhost:5173', process.env.CLIENT_URL as string], // Cho phép truy cập từ origin này
     methods: ['GET', 'POST', 'DELETE'], // Chỉ cho phép các phương thức GET và POST
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'], // Chỉ
     credentials: true
